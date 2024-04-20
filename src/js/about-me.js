c7480ab743e6e07b32a79dad58854223e6c44976
anastasiyaaa0 <anastasiyaaa.kyrylenko@gmail.com>
2024-04-21 00:09:35 +0300

    add about-me swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,11 +1,18 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-const acc = new Accordion('.about-me-info-list.accordion-container', {
-  duration: 300,
-  openOnInit: [0],
-  showMultiple: true,
-});
+import Swiper from 'swiper';
+import 'swiper/css';
+
+// ACCORDEON
+const aboutMeAccordeon = new Accordion(
+  '.about-me-info-list.accordion-container',
+  {
+    duration: 300,
+    openOnInit: [0],
+    showMultiple: true,
+  }
+);
 
 const accordionBtns = document.querySelectorAll(
   '.disclosure-button.ac-trigger'
@@ -19,3 +26,31 @@ function onAccordionBtn(event) {
   const accordionBtnIcon = accordionBtn.firstElementChild;
   accordionBtnIcon.classList.toggle('rotate');
 }
+
+// SWIPER
+const swipeBtn = document.querySelector('.swiper-button');
+const aboutMeSwiper = new Swiper('.about-me-swiper', {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  mousewheel: {
+    invert: true,
+  },
+
+  loop: true,
+  slidesPerView: 2,
+  slideActiveClass: 'is-active',
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1440px
+    1440: {
+      slidesPerView: 6,
+    },
+  },
+});
+
+swipeBtn.addEventListener('click', () => aboutMeSwiper.slideNext());
