ae772d3e1d7123b988623bfbce396db6d3963bef
anastasiyaaa0 <anastasiyaaa.kyrylenko@gmail.com>
2024-04-20 16:54:59 +0300

    about-me  add accordeon

---
+++ src/js/about-me.js
@@ -0,0 +1,29 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const acc = new Accordion('.about-me-info-list.accordion-container', {
+  duration: 300,
+  openOnInit: [0],
+  showMultiple: true,
+});
+
+const accordionBtns = document.querySelectorAll(
+  '.disclosure-button.ac-trigger'
+);
+accordionBtns.forEach(btn => {
+  btn.addEventListener('click', onAccordionBtn);
+});
+
+function onAccordionBtn(event) {
+  const accordionBtn = event.currentTarget;
+  const accordionBtnIcon = accordionBtn.firstElementChild;
+  accordionBtnIcon.classList.toggle('rotate');
+
+  const statusBtn = accordionBtn.dataset.status;
+
+  if (statusBtn === 'open') {
+    accordionBtn.dataset.status = 'close';
+  } else {
+    accordionBtn.dataset.status = 'open';
+  }
+}
